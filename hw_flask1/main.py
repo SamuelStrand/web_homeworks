d1ab61ab73e10c521074c2773def302044587ea1
SamuelStrand <105794723+SamuelStrand@users.noreply.github.com>
2022-11-05 22:31:34 +0600

    uploaded homework with flask

---
+++ hw_flask1/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='template')
+
+
+@app.route("/")
+def main():
+    return render_template('main.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
